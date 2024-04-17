3326deb24b0a8907eb98cf5f5129225ca6ecf5c2
Виктор Кочнев <victor_k02@mail.ru>
2024-04-17 16:58:17 +0300

    добавил больше дефолтных сущностей

--- init-db/init/platform-init.sql
+++ init-db/init/platform-init.sql
@@ -1,3 +1,8 @@
-\i /init/platform-init/001_insert_user.sql;
-\i /init/platform-init/002_insert_plugin.sql;
-\i /init/platform-init/003_insert_plugin_usage.sql;
+\i
+/init/platform-init/001_insert_user.sql;
+\i
+/init/platform-init/002_insert_plugin.sql;
+\i
+/init/platform-init/003_insert_plugin_usage.sql;
+\i
+/init/platform-init/004_insert_tag.sql;
