c52acc891f2e51498b69c3b4d532626af73f96f4
Виктор Кочнев <victor_k02@mail.ru>
2024-04-24 22:30:12 +0300

    поменял версия образа плагина

--- init-db/init/platform-init.sql
+++ init-db/init/platform-init.sql
@@ -1,8 +1,4 @@
-\i
-/init/platform-init/001_insert_user.sql;
-\i
-/init/platform-init/002_insert_plugin.sql;
-\i
-/init/platform-init/003_insert_plugin_usage.sql;
-\i
-/init/platform-init/004_insert_tag.sql;
+\i /init/platform-init/001_insert_user.sql;
+\i /init/platform-init/002_insert_plugin.sql;
+\i /init/platform-init/003_insert_plugin_usage.sql;
+\i /init/platform-init/004_insert_tag.sql;
